c427e42090f60a33298695316c8f34cd57cf6473
jonatasx <jonatas.santospessoa@gmail.com>
2022-10-30 20:39:25 -0300

    commit pra salvar

---
+++ Edka.Sa02/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edka.Sa02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 18
+             * 29
+             * 37
+             * 48
+             * 17
+             * 16
+             * 54
+             * 72
+             * 45
+             * 30
+             */
+            int[] idades = new int[10] { 18, 29, 37, 48, 17, 16, 54, 72, 45, 30 };
+
+            double soma = 0;
+            double media = 0;
+            for (int i = 0; i < idades.Length; i++)
+            {
+                Console.WriteLine(idades[i]);
+                soma += idades[i];
+            }
+            media = soma / 10;
+
+            Console.WriteLine("A media é: " + media);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Edka.Sa03/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edka.Sa03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Fonte de dados(Vetor, Matriz, Collections, .xls, .xml, .txt, .csv, .dat, .sql)
+            //  A fonta escolhida será um vetor.
+
+            int[] votos = new int[10]
+            {
+                1,2,3,4,5,6,7,5,5,7
+        };
+            //2. Consulta : Query Sintax
+            var consulta = from voto in votos
+                           select voto;
+
+            //3. Executando Consulta  
+            // Listando com "foreach"
+            foreach (int  voto in consulta)
+                {
+                Console.WriteLine(voto);
+            }
+
+            //Listando com ToList()
+            var votosFiltrados = consulta.ToList();
+            Console.WriteLine($"Votos Filtrados:{Convert.ToInt32(votosFiltrados)}");
+            Console.ReadKey();
+        }
+    }
+}
